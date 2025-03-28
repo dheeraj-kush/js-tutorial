ac741895c48c058605afb35edd72833cfd124303
dheerajkshwaha <167116148+dheeraj-kush@users.noreply.github.com>
2025-03-28 13:35:51 +0000

    variables

---
+++ variables.js
@@ -0,0 +1,20 @@
+const accountId = 123456
+let accountEmail = "dheerajkushwaha@gmail.com"
+var accountPasseord = "12345"
+accountCity = "Lucknow"
+
+// accountId = 3  not allowed
+
+accountEmail = "dk@Ak.com"
+accountPasseord = "555555"
+accountCity = "Lakhimpur"
+let accountState;
+
+console.log(accountId);
+
+/* 
+prefer not to use var 
+because of issue in block and 
+*/
+
+console.table([accountId, accountEmail, accountPasseord, accountCity, accountState])
