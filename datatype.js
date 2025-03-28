175d65cd1f5a986bb92444f10560c612900ef59e
dheerajkshwaha <167116148+dheeraj-kush@users.noreply.github.com>
2025-03-28 13:36:16 +0000

    datatypes

---
+++ datatype.js
@@ -0,0 +1,16 @@
+ "use strict"; // treat all JS code as newer version
+ 
+//  alert(3+3) // ew are using node js, not browser
+// code readability should be high
+
+// number ==> 2 to power 53
+// bigint 
+// string
+// boolean ==> true/false
+// null
+// undefind ==>
+// symbol => unique
+
+// boject 
+
+console.log(typeof null);
