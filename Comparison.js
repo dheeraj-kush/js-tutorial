b2179be8d9c307077613eee44fe55c8b88ebc8cc
dheerajkshwaha <167116148+dheeraj-kush@users.noreply.github.com>
2025-03-30 16:02:11 +0000

    Comparison operators

---
+++ Comparison.js
@@ -0,0 +1,30 @@
+// Comparison operators =>
+// A comparison operator compares its operands and 
+// returns a logical value based on whether the comparison is true.
+//  The operands can be numerical, string, logical, or object values. 
+// Strings are compared based on standard lexicographical ordering, using Unicode values.
+
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" >0);
+// console.log("02" > 0);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+
+
+
+
+
