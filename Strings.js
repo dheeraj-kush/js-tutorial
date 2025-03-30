5749ea090c570b75df6e1206663ee4d0b8c5e581
dheerajkshwaha <167116148+dheeraj-kush@users.noreply.github.com>
2025-03-30 16:03:48 +0000

    Add Strings  from multipale types

---
+++ Strings.js
@@ -0,0 +1,21 @@
+let str1 = "dheeraj"
+let str2 = " kushwaha"
+let str3 = str1 + str2
+console.log(str3);
+ 
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3+4)*5/3);
+
+
+let num1, num2, num3
+
+num1 = num2 = num = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
